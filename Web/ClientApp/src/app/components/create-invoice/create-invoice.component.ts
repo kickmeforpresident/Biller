5b1af9bf8c454ca5dff1a5a8bd1900fe458a12e9
ikocsis <ikocsis@n-ware.eu>
2019-02-10 15:34:42 +0100

    Set up create invoice markup

--- Web/ClientApp/src/app/components/create-invoice/create-invoice.component.ts
+++ Web/ClientApp/src/app/components/create-invoice/create-invoice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-invoice',
@@ -10,6 +11,20 @@ export class CreateInvoiceComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  createInvoice(form: NgForm) {
+    //let credentials = JSON.stringify(form.value);
+
+    //this.service.login(credentials).subscribe(response => {
+    //  let token = (<any>response).token;
+    //  this.setJWTInLocalStorage(token);
+    //  this.switchInvalidLogin()
+    //  this.service.setIsLoggedIn();
+    //  this.redirectAfterSuccessfulLogin();
+    //}, err => {
+    //  this.switchInvalidLogin()
+    //});
   }
 
 }
