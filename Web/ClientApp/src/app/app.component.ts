4b6919d599542810c69b83117626bcdc03afdff7
ikocsis <ikocsis@n-ware.eu>
2019-02-10 22:38:20 +0100

    Check logged in on startup

--- Web/ClientApp/src/app/app.component.ts
+++ Web/ClientApp/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    
+  title = 'Biller';
+
+  constructor(private service: AuthService) {
+  }
+
+  ngOnInit() {
+    this.service.setIsLoggedIn();
+  }
 }
